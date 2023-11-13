96704e6d43c0854917b365347d77b8c530e34d60
rafikmojr <106155526+rafikmojr@users.noreply.github.com>
2023-11-13 15:59:32 +0200

    Update apis.py

--- apis.py
+++ apis.py
@@ -1,48 +1,43 @@
-from flask import Flask, request, jsonify
-
+from flask import Flask, jsonify, request
 app = Flask(__name__)
 
-# Dummy data for testing
-users_data = [
-    {"email": "user1@example.com", "username": "user1"},
-    {"email": "user2@example.com", "username": "user2"},
-    # Add more users as needed
-]
-
-books_data = [
-    {"book_title": "Book1", "owner": "user1", "secret": "Secret1"},
-    {"book_title": "Book2", "owner": "user2", "secret": "Secret2"},
-    # Add more books as needed
-]
-
-# /createdb
+# Endpoint to create and populate the database with dummy data
 @app.route('/createdb', methods=['GET'])
 def create_db():
-    # Dummy implementation
-    return jsonify({"message": "Creates and populates the database with dummy data"})
+    return jsonify({'message': 'Creates and populates the database with dummy data'})
 
-# /
+# Home endpoint
 @app.route('/', methods=['GET'])
 def home():
-    return jsonify({"message": "VAmPI home", "Help": "Your help information here"})
+    return jsonify({'message': 'VAmPI home', 'Help': 'VAmPI is a vulnerable on-purpose API. It was created to evaluate the efficiency of third-party tools in identifying vulnerabilities in APIs but can also be used for learning/teaching purposes.'})
 
-# /users/v1
+# Users endpoints
 @app.route('/users/v1', methods=['GET'])
 def get_all_users():
-    return jsonify(users_data)
+    return jsonify([{'email': 'user1@example.com', 'username': 'user1'}, {'email': 'user2@example.com', 'username': 'user2'}])
 
-# /users/v1/_debug
 @app.route('/users/v1/_debug', methods=['GET'])
 def debug_users():
-    return jsonify(users_data)
+    return jsonify([{'admin': True, 'email': 'user1@example.com', 'password': 'hashed_password', 'username': 'user1'},
+                    {'admin': False, 'email': 'user2@example.com', 'password': 'hashed_password', 'username': 'user2'}])
 
-# /users/v1/register
 @app.route('/users/v1/register', methods=['POST'])
 def register_user():
-    # Dummy implementation
-    return jsonify({"message": "Successfully created user", "status": "success"})
+    data = request.get_json()
+    username = data.get('username')
+    password = data.get('password')
+    email = data.get('email')
+    # Implement user registration logic
+    return jsonify({'message': 'Successfully created user', 'status': 'success'})
 
-# Add more routes for the remaining paths based on your needs
+# Add other user endpoints
+
+# Books endpoints
+@app.route('/books/v1', methods=['GET'])
+def get_all_books():
+    return jsonify({'Books': [{'book_title': 'Book1', 'user': 'user1'}, {'book_title': 'Book2', 'user': 'user2'}]})
+
+# Add other book endpoints
 
 if __name__ == '__main__':
-    app.run(host='localhost', port=5000, debug=True)
+    app.run(port=5000)
