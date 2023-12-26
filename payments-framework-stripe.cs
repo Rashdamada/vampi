50e2b5148eaaebab6e8d72f5b24f23c8303001c0
rafikmojr <106155526+rafikmojr@users.noreply.github.com>
2023-12-26 12:07:13 +0200

    Create payments-framework-stripe.cs

---
+++ payments-framework-stripe.cs
@@ -0,0 +1,33 @@
+using System;
+using Stripe;
+
+class Program
+{
+  static void Main(string[] args)
+  {
+    StripeConfiguration.ApiKey = "sk_test_4eC39HqLyjWDarjtT1zdp7dc";
+
+    var optionsProduct = new ProductCreateOptions
+    {
+      Name = "Starter Subscription",
+      Description = "$12/Month subscription",
+    };
+    var serviceProduct = new ProductService();
+    Product product = serviceProduct.Create(optionsProduct);
+    Console.Write("Success! Here is your starter subscription product id: {0}\n", product.Id);
+
+    var optionsPrice = new PriceCreateOptions
+    {
+      UnitAmount = 1200,
+      Currency = "usd",
+      Recurring = new PriceRecurringOptions
+      {
+          Interval = "month",
+      },
+      Product = product.Id
+    };
+    var servicePrice = new PriceService();
+    Price price = servicePrice.Create(optionsPrice);
+    Console.Write("Success! Here is your starter subscription price id: {0}\n", price.Id);
+  }
+}
